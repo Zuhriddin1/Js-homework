6931666126f5fd4a0d583af7231b0c32a12049ec
Zuhriddin1 <anarimon1985@gmail.com>
2023-11-10 19:45:55 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// 1-savol
+let a = "10";
+let b = 5;
+let c = a + b;
+console.log(c); // 105
+// NaN
+let d = "2a" - 5;
+console.log(d);
+// 3-savol
+// let f = "10"; 1-xolat
+let f = +"10";
+console.log(f);
+// Boolean
+let v = Boolean("25");
+console.log(v);
+//++
+let s = 77;
+s++;
+console.log(s);
+// --
+let g = 49;
+g++;
+console.log(g);
+// null
+let k = 77;
+k = null;
+console.log(k);
